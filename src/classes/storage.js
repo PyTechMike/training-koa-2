61151819a71ed0b47cdcc072ebbc51a6c243fc89
PyTechMike <pytechmike@gmail.com>
2018-07-30 01:43:16 +0300

    srorage class completed

--- src/classes/storage.js
+++ src/classes/storage.js
@@ -1,3 +1,24 @@
+import fs from '../services/fs.service';
 export default class Storage{
-	
+	constructor(filePass) {
+		this.filePass = filePass || 0;
+	}
+	get(key) {
+		return new Promise(function(resolve, reject) {
+			let value = fs.get(this.filePass, key);
+			resolve(value);
+		})
+	}
+	set(key, value) {
+		return new Promise(function(resolve, reject) {
+			fs.set(this.filePass, key, value);
+			resolve(value);
+		})
+	}
+	remove(key) {
+		return new Promise(function(resolve, reject) {
+			fs.remove(this.filePass, key);
+			resolve();
+		})
+	}
 }
