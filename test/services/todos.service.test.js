b0c8967371d2205102d937927d786d028c095466
PyTechMike <pytechmike@gmail.com>
2018-11-21 23:04:50 +0200

    todes service tests added

--- test/services/todos.service.test.js
+++ test/services/todos.service.test.js
@@ -2,9 +2,11 @@ import todos from '../../src/services/todos.service';
 import Storage from '../../src/classes/storage';
 
 descride('Todos service can', function () {
-	let result;
+	let result, id, todo;
 
 	beforeEach(function () {
+		id = 3;
+		todo = { data: 'some data' };
 		jest.spyOn(Storage.prototype, 'getAll').mockResolvedValue('data');
 		jest.spyOn(Storage.prototype, 'get').mockResolvedValue('data');
 	});
@@ -14,7 +16,22 @@ descride('Todos service can', function () {
 	});
 
 	test('get todo by id', async function () {
-		result = await todos.getAllTodos();
+		result = await todos.getTodoById(id);
+		expect(result).toBe(true);
+	});
+
+	test('post todo', async function () {
+		result = await todos.postTodo(todo);
+		expect(result).toBe(true);
+	});
+
+	test('put todo by id', async function () {
+		result = await todos.putTodo(todo, id);
+		expect(result).toBe(true);
+	});
+
+	test('dalete todo by id', async function () {
+		result = await todos.delTodo(id);
 		expect(result).toBe(true);
 	});
 
